60e90acf76b125c4609b237bd2977a87e067418d
CLAIRESKYLIGHT <bioangela3@gmail.com>
2025-11-24 21:35:45 +0800

    fixed the contractor controller

--- app/Http/Controllers/Api/ContractorController.php
+++ app/Http/Controllers/Api/ContractorController.php
@@ -14,6 +14,7 @@ class ContractorController extends Controller
         $this->middleware('auth:sanctum');
         $this->middleware('role:admin')->except(['index', 'show']);
     }
+
     // Get all contractors
     public function index()
     {
